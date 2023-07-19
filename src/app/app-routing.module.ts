c9a53d5798c60c972dc4a8849740ace694f3ade0
Diego Matias <dg.matias021@gmail.com>
2023-07-19 10:06:36 -0300

    adicionando o sistema de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: BannerComponent},
+  {path: "about", component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
