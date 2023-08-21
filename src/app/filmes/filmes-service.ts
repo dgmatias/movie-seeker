f9b1684911045fee45595c7fd4341d3c18d840cd
Diego Matias <dg.matias021@gmail.com>
2023-08-21 12:49:29 -0300

    adicionando o sistema de filmes por categoria

--- src/app/filmes/filmes-service.ts
+++ src/app/filmes/filmes-service.ts
@@ -3,14 +3,15 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Filme } from "../models/filme-model";
 import { MOVIE_API } from "../app.api";
+import { ActivatedRoute } from "@angular/router";
 
 @Injectable()
 export class FilmesService {
 
     constructor(private http: HttpClient) {};
 
-    listMovie():Observable<Filme[]> {
-        return this.http.get<Filme[]>(`${MOVIE_API}/movies`)
+    listMovie(id: number):Observable<Filme[]> {
+        return this.http.get<Filme[]>(`${MOVIE_API}/movies?categoria=${id}`)
     }
 
 }
--- src/app/filmes/filmes.component.ts
+++ src/app/filmes/filmes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Filme } from '../models/filme-model';
 import { FilmesService } from './filmes-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mov-filmes',
@@ -11,10 +12,10 @@ export class FilmesComponent {
 
   filmes: Filme[] = []
 
-  constructor(private service: FilmesService) {}
+  constructor(private service: FilmesService, private route:ActivatedRoute) {}
 
   ngOnInit() {
-    this.service.listMovie().subscribe(filmes => this.filmes = filmes)
+    this.service.listMovie(this.route.snapshot.params['id']).subscribe(filmes => this.filmes = filmes)
   }
 
 }
